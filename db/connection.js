591787b0cf10e4d73bf23cbd1fb0901ed7016bc3
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 10:39:13 +0000

    Switch to postgreSQL due to mySQL/knex issue with user rights.

---
+++ db/connection.js
@@ -0,0 +1,6 @@
+const ENV = process.env.NODE_ENV || 'development';
+const knex = require('knex');
+const dbConfig =
+  ENV === 'production' ? { client: 'mysql', connection: process.env.DATABASE_URL } : require('../knexfile');
+
+module.exports = knex(dbConfig);
