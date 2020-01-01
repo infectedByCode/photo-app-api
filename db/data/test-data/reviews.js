92b6956fe8e3d7e2a1bf5c42094a07407e31ad52
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 10:59:32 +0000

    Update random images to be more specific

--- db/data/test-data/reviews.js
+++ db/data/test-data/reviews.js
@@ -3,7 +3,7 @@ module.exports = [
     review_title: 'Iste rem ut temporibus aut magni consectetur sunt nihil aliquam.',
     review_body:
       'Quia ea facere in commodi voluptatem corporis expedita. Odio incidunt aspernatur asperiores. Sequi consequatur et voluptates enim neque deleniti accusantium corrupti animi. Reiciendis sunt quaerat perferendis.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 6,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 20
@@ -12,7 +12,7 @@ module.exports = [
     review_title: 'Quod nisi corporis eligendi quasi libero dolores incidunt.',
     review_body:
       'Autem maiores quibusdam doloremque ea et eius et. Blanditiis velit voluptas vel. Dolorem dignissimos ea minus facilis quasi qui est non. Error libero quaerat optio rem placeat fuga fuga. Possimus illo perferendis doloribus quasi minima.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 1,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 14
@@ -21,7 +21,7 @@ module.exports = [
     review_title: 'Tenetur minus eos excepturi aliquid voluptatibus id est quos.',
     review_body:
       'Eligendi error illo natus. Ab deleniti odio qui rerum. Quis laborum et ut aut earum illo sequi atque. Quisquam qui eos culpa qui.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 12,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 18
@@ -30,7 +30,7 @@ module.exports = [
     review_title: 'Cupiditate est omnis in omnis atque vero id veniam.',
     review_body:
       'Provident magni sequi sit molestias dolores veniam. Eius cum voluptatum molestiae pariatur saepe sint nihil. Cupiditate aut recusandae consequuntur architecto.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 8,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 10
@@ -39,7 +39,7 @@ module.exports = [
     review_title: 'Quia quaerat eum dolor delectus.',
     review_body:
       'Aut saepe nihil. In commodi non vero mollitia sint. Qui nihil qui distinctio assumenda quo ut quia. Explicabo possimus voluptatem eos molestias quidem sint quidem voluptatem. Veritatis ipsa in enim asperiores illum.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 12,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 4
@@ -48,7 +48,7 @@ module.exports = [
     review_title: 'Voluptatum sunt amet sunt harum.',
     review_body:
       'Non mollitia enim voluptates officiis illo quo quaerat quo neque. Est animi omnis aspernatur qui sed quis modi. Asperiores ab tempore natus dolor ex ut et. Debitis non aliquam sint et consequatur ab.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 9,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 18
@@ -57,7 +57,7 @@ module.exports = [
     review_title: 'Natus est nam voluptas aut nesciunt quo eveniet nostrum.',
     review_body:
       'Ut sequi consequatur voluptatem ut fuga velit dolor animi nam. Ipsam dignissimos asperiores dolores sed ut aliquid necessitatibus et et. Dolore rerum sapiente aut. Voluptates rerum nihil ducimus sit ut consectetur. Maxime a aut qui cumque nihil architecto ut et hic.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 14,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 18
@@ -66,7 +66,7 @@ module.exports = [
     review_title: 'Et veritatis deserunt explicabo voluptatem.',
     review_body:
       'Quod possimus harum et magnam omnis quasi consequatur deserunt. Sunt placeat ea maiores. Nihil nihil iste in dolor deserunt quisquam facilis id. Eveniet aut asperiores ea. Occaecati repellat dolor laudantium ad illum nemo incidunt ab omnis.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 0,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 11
@@ -75,7 +75,7 @@ module.exports = [
     review_title: 'Aut autem quibusdam ut et.',
     review_body:
       'Facilis consequuntur quis. Aspernatur molestias quibusdam qui reprehenderit et vero et et sunt. Nulla eos sequi omnis et temporibus aut tempora. Voluptatem et ipsam sit vel omnis. Aliquam dolorem facilis cumque neque neque quia.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 13,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 7
@@ -84,7 +84,7 @@ module.exports = [
     review_title: 'Itaque natus perspiciatis occaecati omnis reprehenderit vel eos sint quasi.',
     review_body:
       'Omnis optio et dolorem labore et consequuntur itaque. Cumque quo totam et repudiandae. Ad eveniet itaque incidunt cum dolorem veritatis. Est ea vel.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 8,
     author: '3c9f50cb-da22-4a7d-b105-246b6f14abf4',
     location_id: 16
@@ -93,7 +93,7 @@ module.exports = [
     review_title: 'Aliquid labore enim hic nulla et mollitia dolor.',
     review_body:
       'Tempore est voluptatem. Saepe sit totam suscipit sint nobis qui. Est sint quis ipsa neque iure fuga voluptatem.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 9,
     author: 'fa0963c2-ec9f-4180-9256-0bd756114e90',
     location_id: 11
@@ -102,7 +102,7 @@ module.exports = [
     review_title: 'Quasi consequuntur deleniti dignissimos reiciendis alias magni eum.',
     review_body:
       'Dolorem ab id dicta eveniet molestias temporibus enim. Enim maxime sed error ullam omnis. Recusandae molestiae vero. Sit est rerum sit vero doloribus libero dolorem incidunt sed. Earum et quis aut vel consequatur non suscipit. Ut qui quas praesentium deleniti alias architecto.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 8,
     author: 'fa0963c2-ec9f-4180-9256-0bd756114e90',
     location_id: 18
@@ -111,7 +111,7 @@ module.exports = [
     review_title: 'Ratione ut provident facilis et.',
     review_body:
       'Perferendis odio eligendi aut repellat. Distinctio facere alias. Quia iusto eligendi quia delectus est sit dolores.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 5,
     author: 'fa0963c2-ec9f-4180-9256-0bd756114e90',
     location_id: 2
@@ -120,7 +120,7 @@ module.exports = [
     review_title: 'Facilis voluptatem magni nostrum numquam.',
     review_body:
       'Nihil quaerat et dignissimos nam. Fugiat unde numquam totam totam error. Ut est delectus nulla nulla. Voluptates non quae harum eveniet officia perferendis dignissimos qui ab.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 11,
     author: 'fa0963c2-ec9f-4180-9256-0bd756114e90',
     location_id: 10
@@ -129,7 +129,7 @@ module.exports = [
     review_title: 'Non iure dolore consectetur et occaecati ea tempore ea.',
     review_body:
       'Quam ea necessitatibus velit incidunt magni maiores earum quod. Porro nulla quo. Incidunt voluptatem quas ea inventore id. Minus in autem voluptatum nisi laboriosam sequi doloremque delectus nulla.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 12,
     author: 'fa0963c2-ec9f-4180-9256-0bd756114e90',
     location_id: 9
@@ -138,7 +138,7 @@ module.exports = [
     review_title: 'Repellendus quo asperiores repellendus quibusdam error quo rerum.',
     review_body:
       'Soluta non autem sit deleniti veniam nihil qui rerum. Aut sequi ullam deleniti sequi natus atque ratione dolore odio. Quia soluta harum qui. Atque ipsum veniam excepturi labore qui alias.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 5,
     author: 'de4dffd5-5bd8-47b5-8a5a-7db1efc06a4c',
     location_id: 14
@@ -147,7 +147,7 @@ module.exports = [
     review_title: 'Numquam placeat et magnam consequatur non.',
     review_body:
       'Exercitationem ipsam sint autem aliquam velit id minima aut. Est ipsam officiis tempore corrupti rem. Sequi ullam ipsam eos eius odit fugit veritatis. At iusto tempore non consequatur.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 15,
     author: 'de4dffd5-5bd8-47b5-8a5a-7db1efc06a4c',
     location_id: 19
@@ -156,7 +156,7 @@ module.exports = [
     review_title: 'Velit quo quos dolorem.',
     review_body:
       'A debitis et nemo reiciendis deserunt esse velit cum. Ab iure occaecati corrupti officia commodi. Quos eum quod. Id est modi incidunt mollitia consequatur deserunt.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 6,
     author: 'de4dffd5-5bd8-47b5-8a5a-7db1efc06a4c',
     location_id: 19
@@ -165,7 +165,7 @@ module.exports = [
     review_title: 'Facilis repellat sunt qui.',
     review_body:
       'Quia quasi consequatur. Voluptas eligendi tenetur sunt perferendis dignissimos commodi inventore aperiam. Nemo recusandae qui quibusdam. Dolorum molestiae eum id ipsam cum. Et aut reiciendis et maiores.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 12,
     author: 'de4dffd5-5bd8-47b5-8a5a-7db1efc06a4c',
     location_id: 1
@@ -174,7 +174,7 @@ module.exports = [
     review_title: 'In vero aut et animi veniam qui.',
     review_body:
       'Et cum saepe ipsa vel ut omnis placeat. Aliquid dicta blanditiis neque ut ducimus. Eaque quaerat quisquam dolores error. Doloremque aut tempore. Cum consectetur in dolores accusantium.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 15,
     author: 'de4dffd5-5bd8-47b5-8a5a-7db1efc06a4c',
     location_id: 4
@@ -183,7 +183,7 @@ module.exports = [
     review_title: 'Dolorum quod at autem temporibus exercitationem voluptatem nulla rerum.',
     review_body:
       'Modi molestiae excepturi et molestiae. Sunt eaque quod expedita. Impedit distinctio quas animi fugiat. Aliquam et non modi officiis quo dolorem.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 6,
     author: 'a0b560a4-7d4c-43e1-a094-1d3528ef710f',
     location_id: 12
@@ -192,7 +192,7 @@ module.exports = [
     review_title: 'Veniam tenetur ex laudantium rem iste harum beatae.',
     review_body:
       'Ut vero quia alias sequi. Qui dicta qui dignissimos sint omnis. Tempora et fuga enim accusamus veritatis distinctio sint enim omnis. Suscipit iure necessitatibus praesentium a magnam voluptatem. Quis cumque ut aliquid non labore.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 8,
     author: 'a0b560a4-7d4c-43e1-a094-1d3528ef710f',
     location_id: 18
@@ -201,7 +201,7 @@ module.exports = [
     review_title: 'Velit quibusdam iste nihil fugiat explicabo et qui.',
     review_body:
       'Explicabo quaerat suscipit tempora voluptatem optio tenetur dolor. Exercitationem perspiciatis inventore consequuntur nemo consectetur aperiam vel rem. Voluptate et possimus quis ipsam sequi eius. Doloremque sapiente ipsa et magnam voluptatem et ullam sit.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 14,
     author: 'a0b560a4-7d4c-43e1-a094-1d3528ef710f',
     location_id: 14
@@ -210,7 +210,7 @@ module.exports = [
     review_title: 'Ut enim similique delectus et laborum esse quam perspiciatis.',
     review_body:
       'Nostrum et labore enim quis sint qui sed eveniet. Rerum recusandae repudiandae fugiat soluta aut repellendus saepe. Voluptatem reprehenderit debitis laborum modi autem dolores ut. Optio quia nam repudiandae dicta provident. Blanditiis similique eveniet pariatur ut unde. Esse qui iure molestias.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 8,
     author: 'a0b560a4-7d4c-43e1-a094-1d3528ef710f',
     location_id: 15
@@ -219,7 +219,7 @@ module.exports = [
     review_title: 'Facilis et necessitatibus voluptatem quia.',
     review_body:
       'Quos unde qui dolores. Recusandae et in voluptates non magni quaerat quod. Fugit eum porro voluptatem.',
-    image_url: 'http://lorempixel.com/640/480',
+    image_url: 'http://lorempixel.com/400/400/city/',
     vote_count: 3,
     author: 'a0b560a4-7d4c-43e1-a094-1d3528ef710f',
     location_id: 14
