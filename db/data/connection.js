2082e894c7b70bc3d7e9becee9be35e0cd66205b
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 10:39:33 +0000

    Move connection.js file to db folder.

--- db/data/connection.js
+++
@@ -1,6 +0,0 @@
-const ENV = process.env.NODE_ENV || 'development';
-const knex = require('knex');
-const dbConfig =
-  ENV === 'production' ? { client: 'mysql', connection: process.env.DATABASE_URL } : require('../knexfile');
-
-module.exports = knex(dbConfig);
