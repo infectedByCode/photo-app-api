da0f68b7e444e2a0a658c03c3d7b8658cd470819
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 20:54:42 +0000

    Add continent to seed data

--- db/data/development-data/locations.js
+++ db/data/development-data/locations.js
@@ -1,82 +1,102 @@
 module.exports = [
   {
     city: 'Orlando',
-    country: 'United States of America'
+    country: 'United States of America',
+    continent: 'North America'
   },
   {
     city: 'Sydney',
-    country: 'Australia'
+    country: 'Australia',
+    continent: 'Oceania'
   },
   {
     city: 'London',
-    country: 'United Kingdom'
+    country: 'United Kingdom',
+    continent: 'Europe'
   },
   {
     city: 'Paris',
-    country: 'France'
+    country: 'France',
+    continent: 'Europe'
   },
   {
     city: 'Venice',
-    country: 'Italy'
+    country: 'Italy',
+    continent: 'Europe'
   },
   {
     city: 'Manhatten',
-    country: 'United States of America'
+    country: 'United States of America',
+    continent: 'North America'
   },
   {
     city: 'Cape Town',
-    country: 'South Africa'
+    country: 'South Africa',
+    continent: 'Africa'
   },
   {
     city: 'Las Vegas',
-    country: 'United States of America'
+    country: 'United States of America',
+    continent: 'North America'
   },
   {
     city: 'Rome',
-    country: 'Italy'
+    country: 'Italy',
+    continent: 'Europe'
   },
   {
     city: 'Christchurch',
-    country: 'New Zealand'
+    country: 'New Zealand',
+    continent: 'Oceania'
   },
   {
     city: 'Mount Cook',
-    country: 'New Zealand'
+    country: 'New Zealand',
+    continent: 'Oceania'
   },
   {
     city: 'Berlin',
-    country: 'Germany'
+    country: 'Germany',
+    continent: 'Europe'
   },
   {
     city: 'Dublin',
-    country: 'Ireland'
+    country: 'Ireland',
+    continent: 'Europe'
   },
   {
     city: 'Hong Kong',
-    country: 'China'
+    country: 'China',
+    continent: 'Asia'
   },
   {
     city: 'Beijing',
-    country: 'China'
+    country: 'China',
+    continent: 'Asia'
   },
   {
     city: 'Bangkok',
-    country: 'Thailand'
+    country: 'Thailand',
+    continent: 'Asia'
   },
   {
     city: 'Hanoi',
-    country: 'Vietname'
+    country: 'Vietnam',
+    continent: 'Asia'
   },
   {
     city: 'Hue',
-    country: 'Vietnam'
+    country: 'Vietnam',
+    continent: 'Asia'
   },
   {
     city: 'Barcelona',
-    country: 'Spain'
+    country: 'Spain',
+    continent: 'Europe'
   },
   {
     city: 'Lisbon',
-    country: 'Portugal'
+    country: 'Portugal',
+    continent: 'Europe'
   }
 ];
