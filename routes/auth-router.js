3d463786f445852bb0cb42d69b39e6f2615c4ce0
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 13:18:14 +0000

    Add status 405 error handler

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,6 +1,10 @@
 const authRouter = require('express').Router();
 const { postUser } = require('../controllers/auth-controller');
+const { handle405Errors } = require('../errors');
 
-authRouter.get('/signup', postUser);
+authRouter
+  .route('/signup')
+  .post(postUser)
+  .all(handle405Errors);
 
 module.exports = authRouter;
