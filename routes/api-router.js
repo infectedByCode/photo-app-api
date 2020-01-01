82a622ccbe0b77581ab4f020d655656443a3f2c3
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 20:38:48 +0000

    Link locationsRouter to apiRouter.

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,8 +1,10 @@
 const apiRouter = require('express').Router();
 const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
+const locationRouter = require('./location-router');
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
+apiRouter.use('/locations', locationRouter);
 
 module.exports = apiRouter;
