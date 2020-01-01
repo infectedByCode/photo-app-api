2d00681917cb125fa590bc89818b0b09ee82994a
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 14:35:57 +0000

    Link userRouter to apiRouter

--- routes/api-router.js
+++ routes/api-router.js
@@ -1,6 +1,8 @@
 const apiRouter = require('express').Router();
 const authRouter = require('./auth-router');
+const userRouter = require('./user-router');
 
 apiRouter.use('/auth', authRouter);
+apiRouter.use('/users', userRouter);
 
 module.exports = apiRouter;
