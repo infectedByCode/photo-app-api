916b97bae0f6de9cfdc76b73dd19f8ec7ad26466
Matt Daley <mdaleyhk@gmail.com>
2020-01-01 12:53:35 +0000

    Link error handling to app.js

--- app.js
+++ app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const apiRouter = require('./routes/api-router');
+const { invalidURLError, customErrorHandlers, psqlErrorHandlers, handleServerErrors } = require('./errors');
 
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', apiRouter);
+
+app.all('/*', invalidURLError);
+
+app.use(customErrorHandlers);
+app.use(psqlErrorHandlers);
+app.use(handleServerErrors);
 
 module.exports = app;
