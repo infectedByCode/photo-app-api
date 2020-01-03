58050a3a06003b54d9f398bf6a8662e96816acfc
Matt Daley <mdaleyhk@gmail.com>
2020-01-03 19:30:07 +0000

    Connect comments router to api router.

--- routes/api-router.js
+++ routes/api-router.js
@@ -3,10 +3,12 @@ const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
 const locationRouter = require('./location-router');
 const reviewRouter = require('./review-router');
+const commentRouter = require('./comment-router');
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/locations', locationRouter);
 apiRouter.use('/reviews', reviewRouter);
+apiRouter.use('/comments', commentRouter);
 
 module.exports = apiRouter;
