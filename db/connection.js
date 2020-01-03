72d73cb623ba5d9cde8cb22ee4413ca0513b6bd7
Matt Daley <mdaleyhk@gmail.com>
2020-01-03 20:17:32 +0000

    Change db in knexfile

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,5 @@
 const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const dbConfig =
-  ENV === 'production' ? { client: 'mysql', connection: process.env.DATABASE_URL } : require('../knexfile');
+const dbConfig = ENV === 'production' ? { client: 'pg', connection: process.env.DATABASE_URL } : require('../knexfile');
 
 module.exports = knex(dbConfig);
