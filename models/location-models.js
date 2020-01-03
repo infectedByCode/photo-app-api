5da118ef9d6158dc80bf091988af24fd51134ad2
Matt Daley <mdaleyhk@gmail.com>
2020-01-03 13:19:45 +0000

    Return an error when the location already exists, and correctly format location data

--- models/location-models.js
+++ models/location-models.js
@@ -1,5 +1,5 @@
 const connection = require('../db/connection');
-const { validateQuery, validateStringInput } = require('../utils/utils');
+const { validateQuery, validateStringInput, formatLocation } = require('../utils/utils');
 
 exports.fetchAllLocations = continent => {
   const continents = ['Europe', 'North American', 'South America', 'Asia', 'Oceania', 'Africa', 'Antarctica'];
@@ -18,17 +18,33 @@ exports.fetchAllLocations = continent => {
     });
 };
 
-// post location, util function to capitalise first letter
 exports.createLocation = locationData => {
   let isDataValid = Object.values(locationData).every(data => {
     return validateStringInput(data);
   });
 
-  const { city, country, continent } = locationData;
-
   if (!isDataValid) {
     return Promise.reject({ status: 400, msg: 'Please only input alphanumeric characters and spaces.' });
   }
 
-  return connection('locations').insert({ city, country, continent }, '*');
+  let { city, country, continent } = locationData;
+
+  if (!city || !country || !continent)
+    return Promise.reject({ status: 400, msg: 'Missing data - Please include a city, country and continent.' });
+
+  if (city) city = formatLocation(city);
+  if (country) country = formatLocation(country);
+  if (continent) continent = formatLocation(continent);
+
+  return connection('locations')
+    .select('*')
+    .where({
+      city,
+      country,
+      continent
+    })
+    .then(location => {
+      if (!location.length) return connection('locations').insert({ city, country, continent }, '*');
+      else return Promise.reject({ status: 400, msg: 'Location already exists.' });
+    });
 };
