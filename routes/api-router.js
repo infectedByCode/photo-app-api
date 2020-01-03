a5bf0f3bfd23ba7e50c3c5c730b5b15dd057e18e
Matt Daley <mdaleyhk@gmail.com>
2020-01-03 13:56:54 +0000

    Link reviewRouter to apiRouter

--- routes/api-router.js
+++ routes/api-router.js
@@ -2,9 +2,11 @@ const apiRouter = require('express').Router();
 const authRouter = require('./auth-router');
 const userRouter = require('./user-router');
 const locationRouter = require('./location-router');
+const reviewRouter = require('./review-router');
 
 apiRouter.use('/auth', authRouter);
 apiRouter.use('/users', userRouter);
 apiRouter.use('/locations', locationRouter);
+apiRouter.use('/reviews', reviewRouter);
 
 module.exports = apiRouter;
