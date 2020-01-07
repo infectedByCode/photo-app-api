8640cde7464b0a4738e4f0d90635274b85f858bd
Matt Daley <mdaleyhk@gmail.com>
2020-01-07 14:48:20 +0000

    Revert previous change in cors

--- app.js
+++ app.js
@@ -7,12 +7,6 @@ const { invalidURLError, customErrorHandlers, psqlErrorHandlers, handleServerErr
 app.use(cors());
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use('/api', apiRouter);
 
 app.all('/*', invalidURLError);
